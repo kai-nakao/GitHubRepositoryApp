c3cf8c735042ceba9ef60e6669b6d4835eb06995
kai.nakao <nkkai0803@gmail.com>
2020-12-17 13:56:34 +0900

    Initial Commit

---
+++ GitHubRepositoryApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHubRepositoryApp
+//
+//  Created by 中尾開 on 2020/12/17.
+//  Copyright © 2020 exaple.com. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
