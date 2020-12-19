806430b008f043ebb9522d3f6221081abf7cf52a
kai.nakao <nkkai0803@gmail.com>
2020-12-19 17:46:07 +0900

    viewModelの実装

--- GitHubRepositoryApp/Home/HomeView.swift
+++ GitHubRepositoryApp/Home/HomeView.swift
@@ -9,11 +9,19 @@
 import SwiftUI
 
 struct HomeView: View {
+    @StateObject private var viewModel: HomeViewModel
     @State private var cardViewInputs: [CardView.input] = []
     
     var body: some View {
         
         NavigationView {
+            if viewModel.isLoading {
+                Text("読込中...")
+                    .font(.headline)
+                    .foregroundColor(.gray)
+                    .offset(x: 0, y: -200)
+                    .navigationBarTitle("", displayMode: .inline)
+            }
             ScrollView(showsIndicators: false) {
                 ForEach(cardViewInputs) { input in
                     Button(action: {
@@ -28,7 +36,7 @@ struct HomeView: View {
             .navigationBarTitle("", displayMode: .inline)
             .navigationBarItems(leading: HStack {
                 TextField("検索キーワードを入力", text: $text, oncommit: {
-                    
+                    viewModel.apply(inputs: .oncommit(text: text))
                 })
                 
                 .textFieldStyle(RoundedBorderTextFieldStyle())
--- GitHubRepositoryApp/Home/HomeViewModel.swift
+++ GitHubRepositoryApp/Home/HomeViewModel.swift
@@ -19,9 +19,32 @@ final class HomeViewModel: ObservableObject {
     @Published private var cardViewInputs: [CardView.Input] = []
     @Published var inputText: String = ""
     @Published var isShowError = false
-    @Published var isLording = false
-    @Published var isShoeSheet = false
+    @Published var isLoading = false
+    @Published var isShowSheet = false
     @Published var repositoryUrl: String = ""
     
+    private let apiService: APIServiceType
+    private let onCommitSubject = PassthroughSubject<String, Never>()
+    private let responseSubject = PassthroughSubject<SearchRepositoryResponse, Never>()
     
+    init(apiService: APIServiceType) {
+        self.apiService = apiService
+        bind()
+    }
+    
+    func apply(inputs: Inputs) {
+        switch inputs {
+        case .oncommit(let inputText): onCommitSubject.send(inputText)
+        case .tappedCardView(let urlString):
+            repositoryUrl = urlString
+            isShowSheet = true
+        }
+    }
+    
+    private func bind() {
+        
+        let loadingStartSubscriber = onCommitSubject
+            .map { _ in true }
+            .assign(to: \.isLoading, on: self)
+    }
 }
